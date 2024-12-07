bb80c82e69fe09d76fc7d01899c707be2948be2b
SkyChow8589 <skychow8589@gmail.com>
2024-12-07 02:09:50 -0600

    ReverseLinkedList206

---
+++ Linked_List/ReverseLinkedList206.java
@@ -0,0 +1,15 @@
+package Linked_List;
+
+public class ReverseLinkedList206 {
+    public static ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode next = null;
+        while (head != null) {
+            next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+}
